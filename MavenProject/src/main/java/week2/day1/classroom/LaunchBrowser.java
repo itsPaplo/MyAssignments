ab5a528d2ed68b2f49affc8a74f4483d0c033c91
paplo <paplo@DESKTOP-I4FSC0B>
2023-07-29 20:11:01 +0530

    Week 2 Day 1 - Classroom Activity 1

---
+++ MavenProject/src/main/java/week2/day1/classroom/LaunchBrowser.java
@@ -0,0 +1,16 @@
+package week2.day1.classroom;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/login");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.close();
+
+	}
+	
+}
