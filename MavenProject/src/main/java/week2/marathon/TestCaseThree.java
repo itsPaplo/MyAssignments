8fedf0139bf642c8412de57e16642f3900ef319d
paplo <paplo@DESKTOP-I4FSC0B>
2023-08-03 17:19:40 +0530

    Week 2 - First Marathon - TC03 [Replaced sleep with Explicit wait]

--- MavenProject/src/main/java/week2/marathon/TestCaseThree.java
+++ MavenProject/src/main/java/week2/marathon/TestCaseThree.java
@@ -5,7 +5,9 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestCaseThree {
 
@@ -14,12 +16,16 @@ public class TestCaseThree {
 		driver.get("https://www.pvrcinemas.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		
+
 		//Interacting with Hamburger menu
 		driver.findElement(By.xpath("//div[@class='nav-icon']")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'Movie Lib')]")).click();
-		Thread.sleep(3000);
 		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		WebElement preloader = driver.findElement(By.xpath("//div[@class='ring-svg']"));
+		wait.until(ExpectedConditions.invisibilityOf(preloader));
+		//Thread.sleep(3000);
+
 		//Interacting with Choose your preference pop-up
 		//Handling City Dropdown 
 		driver.findElement(By.xpath("//label[text()='City']/parent::div")).click();
@@ -27,14 +33,14 @@ public class TestCaseThree {
 		WebElement cityelem = driver.findElement(By.xpath("//select[@name='city']"));
 		Select citydd = new Select(cityelem);
 		citydd.selectByVisibleText("Chennai");
-		
+
 		//Handling Genre Dropdown 
 		driver.findElement(By.xpath("//label[text()='Genre']/parent::div")).click();
 		Thread.sleep(400);
 		WebElement genrelem = driver.findElement(By.xpath("//select[@name='genre']"));
 		Select genrdd = new Select(genrelem);
 		genrdd.selectByVisibleText("ANIMATION");
-		
+
 		//Handling Language Dropdown 
 		driver.findElement(By.xpath("//label[text()='Language']/parent::div")).click();
 		Thread.sleep(400);
@@ -43,15 +49,23 @@ public class TestCaseThree {
 		langdd.selectByVisibleText("ENGLISH");		
 
 		driver.findElement(By.xpath("//button[contains(text(),'Apply')]")).click();
-		
+
 		//Waiting for internal load to finish
-		Thread.sleep(5000);
+		//Thread.sleep(5000);
 		
+		preloader = driver.findElement(By.xpath("//div[@class='ring-svg']"));
+		wait.until(ExpectedConditions.invisibilityOf(preloader));		
+
 		//Selecting first lead movie result
 		driver.findElement(By.xpath("(//div[contains(@class,'movie-wrapper')])[1]")).click();
-		Thread.sleep(4000);
+
+		//Thread.sleep(4000);
+		preloader = driver.findElement(By.xpath("//div[@class='ring-svg']"));
+		wait.until(ExpectedConditions.invisibilityOf(preloader));
+
+
 		driver.findElement(By.xpath("//button[contains(text(),'Proceed')]")).click();	
-		
+
 		//Filling out the details in Send request page
 		//Handling Movie dropdown
 		driver.findElement(By.xpath("//label[text()='Movie']/parent::div")).click();
@@ -59,7 +73,7 @@ public class TestCaseThree {
 		WebElement cinemelem = driver.findElement(By.xpath("//option[text()='Select Cinema']/parent::select"));
 		Select cinemdd = new Select(cinemelem);
 		cinemdd.selectByIndex(1);
-		
+
 		//Interacting with Date picker to select only first active element(day)
 		driver.findElement(By.xpath("//label[contains(@class,'datepicker')]/parent::div")).click();
 		Thread.sleep(400);
@@ -71,7 +85,7 @@ public class TestCaseThree {
 		WebElement prftimeelem = driver.findElement(By.xpath("(//option[contains(text(),'AM')])[1]/parent::select"));
 		Select prftmdd = new Select(prftimeelem);
 		prftmdd.selectByIndex(1);
-		
+
 		//Entering number of seats
 		driver.findElement(By.xpath("//label[contains(text(),'Seats')]/preceding-sibling::input")).sendKeys("2");
 		//Entering Name
@@ -86,25 +100,27 @@ public class TestCaseThree {
 		WebElement fnbelem = driver.findElement(By.xpath("(//option[text()='Yes'])[1]/parent::select"));
 		Select fnbdd = new Select(fnbelem);
 		fnbdd.selectByIndex(1);
-		
+
 		//Entering Other Req
 		Thread.sleep(400);
 		driver.findElement(By.xpath("//label[contains(text(),'Other')]/preceding-sibling::input")).sendKeys("Other accomodation");
-		
+
 		//Interacting with copy to self checkbox
 		driver.findElement(By.xpath("//label[contains(@class,'custom-radio')]")).click();
-		
+
 		//clicking Send request button
 		driver.findElement(By.xpath("//button[contains(text(),'SEND REQ')]")).click();		
-		Thread.sleep(600);	
-		
+		//Thread.sleep(600);	
+		preloader = driver.findElement(By.xpath("//div[@class='ring-svg']"));
+		wait.until(ExpectedConditions.invisibilityOf(preloader));		
+
 		//clicking Cancel in OTP pop-up
 		driver.findElement(By.xpath("(//button[text()='CANCEL'])[2]")).click();
 		Thread.sleep(300);
-		
+
 		//Dismissing Success band
 		driver.findElement(By.xpath("//button[text()='×']")).click();
-		
+
 		//Fetching page current title
 		String pgtitle = driver.getTitle();
 		System.out.println("The page title after dismissing pop-up is: " + '"' + pgtitle + '"');
