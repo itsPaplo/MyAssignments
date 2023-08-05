1fdcdf79a1b143a3fbe1726350fe0db309dda364
paplo <paplo@DESKTOP-I4FSC0B>
2023-08-06 01:12:01 +0530

    Week 3 - Day 1 - Activity 5

---
+++ MavenProject/src/main/java/week3/day1/assignments/assignment5/Automation.java
@@ -0,0 +1,32 @@
+package week3.day1.assignments.assignment5;
+
+public class Automation extends MultipleLangauge implements Language,TestTool{
+
+	public void Selenium() {
+		
+		System.out.println("Method 02: called from Interface Selenium");
+		
+	}
+
+	public void Java() {
+		
+		System.out.println("Method 01: called from Interface Language");
+		
+	}
+
+	@Override
+	public void ruby() {
+		
+		System.out.println("Method 04: abstract method called from Abstract class MultipleLanguages");
+		
+	}
+	
+	public static void main(String[] args) {
+		Automation auto = new Automation();
+		auto.Java();
+		auto.Selenium();
+		auto.python();
+		auto.ruby();
+	}
+
+}
---
+++ MavenProject/src/main/java/week3/day1/assignments/assignment5/MultipleLangauge.java
@@ -0,0 +1,10 @@
+package week3.day1.assignments.assignment5;
+
+public abstract class MultipleLangauge {
+	public void python(){
+		System.out.println("Method 03: called from Abstract class "+ '"' + "python class" + '"' );
+	}
+	
+	public abstract void ruby();
+
+}
