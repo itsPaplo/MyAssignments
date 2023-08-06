59f729009c7c65e8d7e2a38875d1475918e96f4d
paplo <paplo@DESKTOP-I4FSC0B>
2023-08-06 21:18:05 +0530

    Week 3 - Day 2 - Assignments 1.4

--- MavenProject/src/main/java/week3/day2/assignments/PrintUniqueCharacter.java
+++ MavenProject/src/main/java/week3/day2/assignments/PrintUniqueCharacter.java
@@ -6,18 +6,37 @@ import java.util.TreeSet;
 
 public class PrintUniqueCharacter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		/*
+		 * Psuedocode
+		 * 
+		 * a) Convert String to Character array
+		 * b) Create a new Set -> HashSet
+		 * c) Add each character to the Set and if it is already there, remove it
+		 * d) Finally, print the set
+		 * 
+		 */
+		
 		String word = "Philip";
-		char[] carr = word.toLowerCase().toCharArray();		
+		
+		//a) Convert String to Character array
+		char[] carr = word.toLowerCase().toCharArray();	
+		
+		//b) Create a new Set -> HashSet
 		Set<Character> cset = new HashSet<Character>();
 		Set<Character> cdupset = new TreeSet<Character>();
+		
+		//c) Add each character to the Set and if it is already there, remove it
 		for(int i=0;i<carr.length;i++) {
 			if(!(cset.add(carr[i]))) {
 				cdupset.add(carr[i]);
 			}			
 		}		
 		cset.removeAll(cdupset);
+		
+		
 		System.out.print("Unique characters from the given string " + '"' + word + '"' + " are : ");
+		//d) Finally, print the set
 		for(char c : cset) {
 			System.out.print(c + " ");
 		}
