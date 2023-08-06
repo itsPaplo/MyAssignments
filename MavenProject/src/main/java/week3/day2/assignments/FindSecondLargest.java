2c4e17ca38ee875b57b270dab032d2ad5a6b3544
paplo <paplo@DESKTOP-I4FSC0B>
2023-08-06 21:09:22 +0530

    Week 3 - Day 2 - Assignments 1.1

--- MavenProject/src/main/java/week3/day2/assignments/FindSecondLargest.java
+++ MavenProject/src/main/java/week3/day2/assignments/FindSecondLargest.java
@@ -8,15 +8,32 @@ import java.util.TreeSet;
 public class FindSecondLargest {
 
 	public static void main(String[] args) {
+		
+		/*
+		 * Pseudo Code: 
+		 * a) Create a empty Set Using TreeSet
+		 * b) Declare for loop iterator from 0 to data.length and add into Set 
+		 * c) converted Set into List
+		 * d) Print the second last element from List
+		 * 
+		 * 
+		 */
 
 		int[] data = {3,2,11,4,6,7,2,3,3,6,7};
+		
+		//a) Create a empty Set Using TreeSet
 		Set<Integer> numset = new TreeSet<Integer>();
 		List<Integer> numlist = new ArrayList<Integer>();
+		
+		//b) Declare for loop iterator from 0 to data.length and add into Set
 		for(int i=0;i<data.length;i++) {
 			numset.add(data[i]);
 		}
+		
+		//c) converted Set into List
 		numlist.addAll(numset);
 
+		//d) Print the second last element from List
 		System.out.println("The second Largest element in the given integer array is: " + numlist.get(numlist.size()-2));				
 	}
 }
