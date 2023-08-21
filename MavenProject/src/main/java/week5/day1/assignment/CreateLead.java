a6fe27898d013021276a56c16d20dd91c05020e7
paplo <paplo@DESKTOP-I4FSC0B>
2023-08-22 03:57:54 +0530

    Week 5 - Day 1 - Assignment

---
+++ MavenProject/src/main/java/week5/day1/assignment/CreateLead.java
@@ -0,0 +1,26 @@
+package week5.day1.assignment;
+
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CreateLead extends BaseClass{
+
+	@Test
+	public void runCreate() {		
+
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+
+	}
+}
+
+
+
+
+
+
