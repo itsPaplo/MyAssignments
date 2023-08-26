6fd93ad3b010a9b5c59ef1adb2c4db3010f51592
paplo <paplo@DESKTOP-I4FSC0B>
2023-08-26 20:23:23 +0530

    Week 6 - Day - Classroom

---
+++ MavenProject/src/main/java/week6/day1/classroom/CreateLead.java
@@ -0,0 +1,27 @@
+package week6.day1.classroom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class CreateLead extends BaseClass{
+	
+	@BeforeTest
+	public void setSheet() {
+		sheetname = "names";
+	}
+
+	@Test(dataProvider = "fetchData")
+	public void runCreate(String cname, String fname, String lname) {		
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.linkText("Leads")))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.linkText("Create Lead")))).click();
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("createLeadForm_companyName")))).sendKeys(cname);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("createLeadForm_firstName")))).sendKeys(fname);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("createLeadForm_lastName")))).sendKeys(lname);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.name("submitButton")))).click();
+	}
+}
+
+
+
