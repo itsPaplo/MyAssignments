722319314365bab7fc4b19fed602a42fcca1dbd5
paplo <paplo@DESKTOP-I4FSC0B>
2023-08-27 10:40:16 +0530

    Week 6 - Day 2 - Classroom 1

---
+++ MavenProject/src/main/java/week6/day2/classroom/ReadExcelData.java
@@ -0,0 +1,30 @@
+package week6.day2.classroom;
+
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelData {
+	public static String[][] readExcelData(String filename) throws IOException{
+		
+		XSSFWorkbook wb = new XSSFWorkbook("./testdata/"+filename+".xlsx");
+		XSSFSheet sheet = wb.getSheetAt(0);
+		int rowCount = sheet.getLastRowNum();
+		int colCount = sheet.getRow(rowCount).getLastCellNum();
+		
+		String[][] data = new String[rowCount][colCount];
+		for(int i=1;i<=rowCount;i++) {
+			for(int j=0;j<colCount;j++) {
+				String cellData = sheet.getRow(i).getCell(j).getStringCellValue();
+				data[i-1][j] = cellData;
+			}
+		}
+		
+		wb.close();
+		return data;
+		
+	}
+		
+	
+	
+}
